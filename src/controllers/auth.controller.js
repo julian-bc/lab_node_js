e81e02c71269ca3cb400a7ca1f5800576a38dffa
Julian Enrique <julianbuitragocharry2006@gmail.com>
2025-09-09 13:51:31 -0500

    add bcrypt dependency

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,37 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
-  
+  const userSaved = undefined;
+
+  const passwordHashed = bcrypt.hash(password, 10);
+
   const newUser = new User({
     username,
     email,
-    password
+    password: passwordHashed
   });
 
   try {
     console.log(`${newUser} \n saved!`);
-    // await newUser.save();
-  } catch (error) {
+    userSaved = await newUser.save();
+  }
+  
+  catch (error) {
     console.error(error);
     res.status(500).send("Error is happened");
   }
-  
+
   console.log(newUser);
-  res.send("register");
+  res.json({
+    id: userSaved._id,
+    username: userSaved.username,
+    email: userSaved.email,
+    createdAt: userSaved.createdAt,
+    modifyAt: userSaved.modifyAt,
+  });
+  
 }
 
 export const login = (req, res) => {
